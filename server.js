60917498a3a5e3754e6d9acb749a8f37107a0955
Oluwaseun Jayeoba <seunjay92@gmail.com>
2021-07-28 21:07:57 +0100

    use staff route in middleware

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ import morgan from 'morgan';
 
 import connectDB from './config/db.js';
 
+import staffRoutes from './routes/staffRoutes.js';
+
 dotenv.config();
 
 connectDB();
@@ -17,21 +19,19 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-
 app.use(cors());
 
 app.use(express.json());
 
+app.use('/api/staff', staffRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-
 app.listen(
-    PORT,
-    console.log(
-      `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
-    )
-  );
-  
-  export default app;
-  
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+);
+
+export default app;
