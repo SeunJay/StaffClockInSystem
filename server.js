1f35784e33af33c94ffa3fd330a198749b9f858f
Oluwaseun Jayeoba <seunjay92@gmail.com>
2021-07-28 20:30:43 +0100

    connect to mongodb in the cloud

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import colors from 'colors';
 import morgan from 'morgan';
 
+import connectDB from './config/db.js';
+
 dotenv.config();
 
+connectDB();
 
 const app = express();
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+
+app.use(cors());
+
+app.use(express.json());
 
 
 const PORT = process.env.PORT || 5000;
