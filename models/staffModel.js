f7e70a221cbc16efd67e030c005e48705d6bf540
Oluwaseun Jayeoba <seunjay92@gmail.com>
2021-07-28 20:41:21 +0100

    create model for staff

---
+++ models/staffModel.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+
+const staffSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  department: {
+    type: String,
+    required: true,
+  },
+  designation: {
+    type: String,
+    required: true,
+  },
+  isAvailable: {
+    type: Boolean,
+    default: false,
+  },
+  timeIn: {
+    type: Date,
+  },
+
+  timeOut: {
+    type: Date,
+  },
+});
+
+const Staff = mongoose.model('Staff', staffSchema);
+
+export default Staff;
